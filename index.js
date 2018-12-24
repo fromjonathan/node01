a4005b9ef1ae679bcc0a66abe748b639a3cd4153
fromjonathan <bringjd@gmail.com>
2018-12-24 11:00:49 +0800

    nodejs

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // first test
+'use strict'; 
+let http = require('http'); 
+http.createServer(function (req, res) { 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('hello'); 
+}).listen(3000);
